ad70fd643c05bc21bf57c2cb136b59b2190636db
Carlos Villalba <carlos.villalba.95@gmail.com>
2017-07-08 13:07:35 -0500

    subir primer pausa de la clase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DishListComponent } from './dish-list/dish-list.component';
+import { DishListItemComponent } from './dish-list-item/dish-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DishListComponent,
+    DishListItemComponent
   ],
   imports: [
     BrowserModule
